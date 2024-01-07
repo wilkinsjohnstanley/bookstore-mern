f2025ea5aaa3954c054a8f85032b1163191ce5b6
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-07 13:44:02 -0600

    third commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
-import { PORT, mongoDBURL } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+
+
 const app = express();
 
 // be careful not to use backslash, use forward slash!!
@@ -8,6 +11,36 @@ app.get('/', (request, response)=> {
    console.log(request) 
    return response.status(234).send('Hi mom');
 });
+
+
+//Route for Save a red Book
+app.post('/books', async(request, response)=>{
+    try{
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+        return response.status(400).setDefaultEncoding({
+            message:'Send all required fields: title, author, publishYear',
+        });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+
+
 
 //be sur you change directory before npm run dev
 mongoose 
