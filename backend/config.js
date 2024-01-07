07825cee039f994d13996a391c18bda8d3781c06
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-07 12:26:34 -0600

    third commit

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://books:<books>@bookstore-mern.tkkwhwv.mongodb.net/books-collection?retryWrites=true&w=majority';
+export const mongoDBURL = 'mongodb+srv://books:<books>@bookstore-mern.tkkwhwv.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -9,17 +9,16 @@ app.get('/', (request, response)=> {
    return response.status(234).send('Hi mom');
 });
 
-
+//be sur you change directory before npm run dev
 mongoose 
     .connect(mongoDBURL)
-        .then(()=>{
+        .then(()=> {
             console.log('App is connected to the datebase');
             app.listen(PORT, ()=>{
                 //don't confuse `` for ''
                 console.log(`App is listening to port: ${PORT}`);
             });
         })
-        
         .catch((error)=>{
             console.log(error);
         });
